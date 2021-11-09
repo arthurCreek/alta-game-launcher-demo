91c20413bec71252f3874d302697e53f6228ba63
Arturo Arroyo <turo310@gmail.com>
2021-11-08 23:14:09 -0800

    Update home page to open game executable

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 // import { shell } from 'electron';
+// import { ipcRenderer } from 'electron';
+// import * as child from 'child_process';
+import child = require('child_process');
 
 const Home: React.FC = () => {
-  // const handlePlay = () => {
-  //   shell.openPath("C:\\Games\\Alta\\A Township Tale\\A Township Tale.exe")
-  //   // shell.openExternal("https://www.google.com")
-  // }
+  const handlePlay = () => {
+    const child_p = child.execFile;
+    child_p(
+      'C:\\Games\\Alta\\A Township Tale\\A Township Tale.exe',
+      [],
+      function (err: any, data: any) {
+        console.log(err);
+        console.log(data.toString());
+      }
+    );
+  };
 
   return (
     <>
@@ -17,10 +27,10 @@ const Home: React.FC = () => {
       ></div>
       <div className="w-full alta-min-height-full header-height-margin flex z-10">
         <div className="w-128 h-full alta-play-bg alta-min-height-full fixed">
-          {/* <button onClick={handlePlay}>Play</button> */}
-          <a href="C:\\Games\\Alta\\A Township Tale\\A Township Tale.exe">
+          <button onClick={handlePlay}>Play</button>
+          {/* <a href="C:\\Games\\Alta\\A Township Tale\\A Township Tale.exe">
             Link
-          </a>
+          </a> */}
           <p>sdsdkjcbnsdijkcnbdkjcnkdsncskdjcn</p>
         </div>
         <div className="flex-shrink w-home-right max-height-with-header ml-auto mr-0 overflow-y-auto z-10">
